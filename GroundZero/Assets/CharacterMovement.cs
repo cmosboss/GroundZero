61935874950e638364293c2bc4e285bb456d9660
Taylor Moss <Calvintmoss@gmail.com>
2015-11-18 00:01:59 -0700

    Removed VSync, Added default ground (screenshot of the real good-looking asset that will come soon, $2), Added default character WITH animations ~30 (right now he only uses idle 1 at half speed on loop), also setup camera and default scene.

---
+++ GroundZero/Assets/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+    Animator animator;
+    public float speed = 1.0F;
+    public float rotationSpeed = 100.0F;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animator.SetFloat("Speed", 1);
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
