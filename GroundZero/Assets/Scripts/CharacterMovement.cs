96d30d84ecda8cb2765c49ebabab07861801f5e6
Taylor Moss <Calvintmoss@gmail.com>
2015-11-18 20:55:17 -0700

    Added some materials, added animations for attack, added weapon and added newb to 1 hit

---
+++ GroundZero/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+    Animator animator;
+    public float speed = 1.0f;
+    public float rotationSpeed = 100.0F;
+    private bool canMove = true;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    IEnumerator delayCanMove()
+    {
+        yield return new WaitForSeconds(0.15f);
+        canMove = false;
+        animator.SetBool("Heavy Attack", false);
+        yield return new WaitForSeconds(0.75f);
+        canMove = true;
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+        if (canMove == true){
+            if (Input.GetMouseButtonDown(0)){
+                animator.SetBool("Heavy Attack", true);
+                StartCoroutine(delayCanMove());
+            }
+            float translation = Input.GetAxis("Vertical") * speed;
+            animator.SetFloat("Speed", translation);
+            translation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
+        }
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+    }
+}
