fbac36f28927918339175155ab213cde3ad26ea5
Calvin Demamp <calvintmoss@gmail.com>
2015-11-20 21:42:31 -0700

    Added basic animator on NPCs, also added super basic pathfinding, will replace with a* soon. Also a little organization

---
+++ GroundZero/Assets/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	Animator animator;
+	Transform target;
+	public float speed = 2.5f;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		target = GameObject.FindWithTag("Player").transform;
+
+	}
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		animator.SetFloat("Speed", 1);
+		Vector3 targetDir = target.position - transform.position;
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+		transform.rotation = Quaternion.LookRotation(newDir);
+	}
+
+}
--- GroundZero/Assets/Scripts/GetTurnt.cs
+++ GroundZero/Assets/Scripts/GetTurnt.cs
@@ -5,15 +5,18 @@ public class GetTurnt : MonoBehaviour {
     private Rigidbody rb;
     // Use this for initialization
     void Start () {
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+
     }
     public void getHit(Vector3 hitlocation) {
         rb = GetComponent<Rigidbody>();
         rb.AddRelativeForce(Vector3.back * 3000);
         // rb.AddRelativeForce(hitlocation * 1000);
+
     }
 
 }
